48c82f57df202696a8f98a156efba25b3318eb03
SzamockiP <szamocki.piotr04@gmail.com>
2025-04-08 22:59:34 +0200

    Updated sphere hit

--- sphere.c
+++ sphere.c
@@ -4,16 +4,17 @@
 #include <stdbool.h>
 
 double hit_sphere(const Point3 center, double radius, const Ray *ray){
-    Vec3   oc =     vec3_sub_vv(center, ray->origin);
-    double a =      vec3_dot_vv(ray->direction, ray->direction);
-    double b =      -2.0 * vec3_dot_vv(ray->direction, oc);
-    double c =      vec3_dot_vv(oc, oc) - (radius * radius);
-    double disc =   (b * b) - (4 * a * c);  
+    Vec3 oc     = vec3_sub_vv(center,ray->origin);
+    double a    = vec3_lenght_squared(ray->direction);
+    double h    = vec3_dot_vv(ray->direction,oc);
+    double c    = vec3_lenght_squared(oc) - (radius * radius);
+    double disc = (h*h) - (a*c);
 
     if (disc < 0){
         return -1.0;
     }
     else{
-        return (-b - sqrt(disc)) / (2.0 * a);
+        return (h - sqrt(disc)) / a;
     }
+
 }
